6353e25bc861b8bf29e18d59f1261658868deba9
Celia <celia.falconlozano@hotmail.com>
2023-04-27 10:24:07 +0200

    dev(signup): added new user

--- pages/api/auth/signup.tsx
+++ pages/api/auth/signup.tsx
@@ -1,11 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next"; 
 import validator  from "validator";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient();
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ){
     if(req.method === 'POST') {
+        
         const {firstName, lastName, email, city, phone, password} = req.body;
         const errors: String[] = [];
         const validationSchema = [
@@ -43,16 +48,39 @@ export default async function handler(
                 errorMessage: 'Password is not strong enough, add capitalization, numbers and symbols'
             }
         ];
+        
         validationSchema.forEach((check) => {
             if(!check.valid) {
                 errors.push(check.errorMessage)
             }
         })
+
         if(errors.length) {
             return res.status(400).json({errorMessage: errors[0]})
         }
+
+        const userWithEmail = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+        if(userWithEmail) {
+            return res.status(400).json({errorMessage: "Email is asociated with another account"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10) 
+         const user = await prisma.user.create({
+            data: {
+                first_name: firstName,
+                last_name: lastName,
+                password: hashedPassword,
+                city,
+                phone,
+                email
+            }
+        }) 
         res.status(200).json({
-            hello: firstName
+            hello: user
         })
     }
 }
